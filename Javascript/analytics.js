4cedcbd2a4ecc87586b4bf6610dc447cdcb5472c
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2014-02-09 15:42:08 -0800

    test

--- Javascript/analytics.js
+++ Javascript/analytics.js
@@ -5,3 +5,14 @@
 
   ga('create', 'UA-47860622-1', 'neelayjunnarkar.github.io');
   ga('send', 'pageview');
+  
+  ar _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-xxxxx-y']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+
+ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
