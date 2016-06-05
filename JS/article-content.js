25a3d30a80f0620c233c79c81e95ca1d176c5dfd
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-06-05 15:18:06 -0700

    added keyboard post; default block closed except in artwork; show more/show less; changed fonts'

--- JS/article-content.js
+++ JS/article-content.js
@@ -22,34 +22,48 @@ function centerImgs() {
         document.getElementsByTagName('img')[i].style.verticalAlign = 'middle';
 }
 
-//last 11 characters
+//last 11 characters of eye and eye-crossed images
 var eyecrossed = 'crossed.png'
 var eye = 'mgs/eye.png'
 
 function setEyes() {
-    for (var i = 0; i < document.getElementsByClassName('article').length; ++i) {
+    for (var i = 0; i < document.getElementsByClassName('article').length; ++i) { // Loop through all articles
         var article = document.getElementsByClassName('article')[i];
         if (article === document.getElementById('Programs Statement'))
             continue;
-        var icon = article.children[0].children[0];
-        var img = icon.children[0].children[0];
-        img.src = '/Imgs/eye.png'
+        var article_title = article.children[0];
+        var img;
+        for (var j = 0; j < article_title.children.length; ++j) { // Loop in case there are links or other html in the title
+            if (article_title.children[j].classList.contains('subtitle-icon')) {
+                img = article_title.children[j].children[0].children[0];
+                if (!img.src.includes('github.png')) { // do not set the github icon to an eye
+                    img.src = '/Imgs/eye.png';
+                }
+            }
+        }
     }
 }
 
 //have to make it so that doesnt try and switch id Program Statements
 function setEyesCrossed() {
-    for (var i = 0; i < document.getElementsByClassName('article').length; ++i) {
+    for (var i = 0; i < document.getElementsByClassName('article').length; ++i) { // Loop through all articles
         var article = document.getElementsByClassName('article')[i];
         if (article === document.getElementById('Programs Statement'))
             continue;
-        
-        var icon = article.children[0].children[0];
-        var img = icon.children[0].children[0];
-        img.src = '/Imgs/eye-crossed.png';
+        var article_title = article.children[0];
+        var img;
+        for (var j = 0; j < article_title.children.length; ++j) { // Loop in case there are links or other html in the title
+            if (article_title.children[j].classList.contains('subtitle-icon')) {
+                img = article_title.children[j].children[0].children[0];
+                if (!img.src.includes('github.png')) { // do not set the github icon to an eye
+                    img.src = '/Imgs/eye-crossed.png';
+                }
+            }
+        }
     }
 }
 
+// Switch crossed eye to eye, or eye to crossed eye
 function toggleEye(id) {
     var article = document.getElementById(id);
     var article_title = article.children[0];
@@ -66,6 +80,7 @@ function toggleEye(id) {
 
 }
 
+// Switch from summary to content or content to summary
 function toggleContent(id) {
 
     var article = document.getElementById(id);
